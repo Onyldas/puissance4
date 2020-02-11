2f7c91b953eade61913d4d1fb03a433e512737dd
onyldas <onyldas@gmail.com>
2020-02-11 17:25:44 +0100

    Correction code et implémentation diagonales

--- main.py
+++ main.py
@@ -3,11 +3,49 @@ from partie import Partie
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template('index.html',title='Puissance 4')
+    return render_template('index.html', title='Puissance 4')
+
 
 if __name__ == "__main__":
-   # app.run()
+    # app.run()
     game = Partie(15, 14, 8)
     game.begin_game()
+
+    # TESTS RECUPERATION DIAGONALES
+    # test_grille = []
+    # c = 10
+    # for i in range(7):  # n
+    #     test_grille.append([c + 1, c + 2, c + 3, c + 4, c + 5, c + 6])  # m
+    #     c += 10
+    #
+    #
+    # def afficher_grille(grille):
+    #     for e in range(7):
+    #         print(grille[e])
+    #
+    #
+    # afficher_grille(test_grille)
+    # diag = []
+    # y = 6
+    # x = 5
+    # for j in range(7):
+    #     for i in range(6):
+    #         for k in range(-min(x, y), 7 - max(x, y)):
+    #             if i == x + k and j == y + k:
+    #                 diag.append(test_grille[j][i])
+    #
+    # print()
+    # print(diag)
+    #
+    # diag2 = []
+    # for j2 in range(7):
+    #     for i2 in range(6):
+    #         for k2 in range(-min(x+6, y), 7 - max(x-6, y)):
+    #             if 6 - i2 == 8 - x + k2 and j2 == y + k2:
+    #                 diag2.append(test_grille[j2][i2])
+    #
+    # print()
+    # print(diag2)
--- partie.py
+++ partie.py
@@ -10,9 +10,9 @@ class Partie:
     is_ended = False
 
     def __init__(self, m, n, p):
-        self.m = 7
-        self.n = 7
-        self.p = 4
+        self.m = 6  # colonnes
+        self.n = 7  # lignes
+        self.p = 4  # nombres de pions à aligner pour gagner avec p < (min(m,n) - 1)
         logging.set_log('Nouvelle partie : ' + str(self.m) + ' de longueur et ' + str(
             self.n) + ' de largeur.\nLes joueurs doivent aligner ' + str(self.p) + ' jetons.')
         self.creer_tableau()
@@ -25,6 +25,25 @@ class Partie:
         for i in range(self.n):
             print(self.grille[i])
 
+    def get_diagonal_gd(self, x, y):
+        diag = []
+        for j in range(self.n):
+            for i in range(self.m):
+                for k in range(-min(x, y), max(self.n, self.m) - max(x, y)):
+                    if i == x + k and j == y + k:
+                        diag.append(self.grille[j][i])
+        return diag
+
+    def get_diagonal_dg(self, x, y):
+        diag2 = []
+        for j2 in range(self.n):
+            for i2 in range(self.m):
+                for k2 in range(-min(x + self.m-1, y), max(self.n, self.m) - max(x - self.m-1, y)):
+                    if self.m-1 - i2 == self.m + 1 - x + k2 and j2 == y + k2:
+                        diag2.append(self.grille[j2][i2])
+        return diag2
+
+
     def combo_for_user(self, current_y, player, x):
         # Si on est player 1, on met un 1 dans le tableau ou le pion tombera
         if player == 1:
@@ -34,24 +53,55 @@ class Partie:
             self.grille[current_y][int(x) - 1] = 2
 
     def check_victory(self, current_y, player, x):
+        # Vérification des pions successifs sur la ligne horizontale
         current_align_count = 1
-        # vérification des pions successifs sur la ligne horizontale
-        for i in range(1, self.m - 1):
+        for i in range(1, self.m):
             if self.grille[current_y][i] == player and self.grille[current_y][i - 1] == player:
                 # Si deux pions se suivent, on incrémente le compteur
                 current_align_count += 1
-        if current_align_count >= self.p:
-            print("player " + str(player) + " won the game!")
-            return True
+                if current_align_count >= self.p:
+                    print("Player " + str(player) + " won the game! - horizontal")
+                    return True
+            else:
+                current_align_count = 1
+
+        # Vérification des pions successifs sur la ligne verticale
         current_align_count = 1
-        # vérification des pions successifs sur la ligne verticale
-        for i in range(1, self.n):
-            if self.grille[i][int(x) - 1] == player and self.grille[i - 1][int(x) - 1] == player:
+        for j in range(1, self.n):
+            if self.grille[j][int(x) - 1] == player and self.grille[j - 1][int(x) - 1] == player:
                 # Si deux pions se suivent, on incrémente le compteur
                 current_align_count += 1
-        if current_align_count >= self.p:
-            print("player " + str(player) + " won the game!")
-            return True
+                if current_align_count >= self.p:
+                    print("Player " + str(player) + " won the game! - vertical")
+                    return True
+            else:
+                current_align_count = 1
+
+        # Vérification des pions successifs sur la diagonale hautgauche vers basdroite
+        current_align_count = 1
+        diag = self.get_diagonal_gd(int(x), current_y+1)  # on récupère la première diagonale
+        for i in range(1, len(diag)):
+            if diag[i] == player and diag[i - 1] == player:
+                # Si deux pions se suivent sur la diagonale, on incrémente le compteur
+                current_align_count += 1
+                if current_align_count >= self.p:
+                    print("Player " + str(player) + " won the game! - diagonal_gd")
+                    return True
+            else: # si les pions ne se suivent pas, on remet le compteur à 1
+                current_align_count = 1
+
+        # Vérification des pions successifs sur la diagonale hautdroite vers basgauche
+        current_align_count = 1
+        diag2 = self.get_diagonal_dg(int(x)+1, current_y)  # on récupère la deuxième diagonale
+        for i in range(1, len(diag2)):
+            if diag2[i] == player and diag2[i - 1] == player:
+                # Si deux pions se suivent sur la diagonale, on incrémente le compteur
+                current_align_count += 1
+                if current_align_count >= self.p:
+                    print("Player " + str(player) + " won the game! - diagonal_dg")
+                    return True
+            else:  # si les pions ne se suivent pas, on remet le compteur à 1
+                current_align_count = 1
 
     def begin_game(self):
         selected_game = input("Select game : 1- Joueur vs Joueur || 2- Joeur vs Dumb IA || 3 - incoming...")
@@ -61,14 +111,16 @@ class Partie:
 
                 choosen_column = input("Selectionner la colonne:")
                 if (not choosen_column == "") and (not int(choosen_column) > self.m):
-                    current_y = self.m - 1
+                    current_y = self.n - 1
                     # On fait tomber le pion au dessus du précédent si jamais un pion est déjà posé sur cette colonne
                     while self.grille[current_y][int(choosen_column) - 1] == 1 or self.grille[current_y][
                         int(choosen_column) - 1] == 2:
                         current_y -= 1
-                    if current_y < 1:
-                        print("You can't place anymore here")
-                    else:
+                        if current_y < 0:
+                            print("You can't place anymore here")
+                            break
+
+                    if current_y > -1:
                         # On place le pion
                         self.combo_for_user(current_y, current_player, choosen_column)
                         # On affiche la grille
@@ -80,8 +132,8 @@ class Partie:
                             current_player = 2
                         else:
                             current_player = 1
-            else:
-                print("you have to enter a number")
+                else:
+                    print("you have to enter a number")
         if int(selected_game) == 2:
             current_player = 1
             while not self.is_ended:
@@ -89,7 +141,7 @@ class Partie:
                     choosen_column = input("Selectionner la colonne:")
                     if (not choosen_column == "") and (not int(choosen_column) > self.m):
                         if not int(choosen_column) > self.m:
-                            current_y = self.m - 1
+                            current_y = self.n - 1
                             # On fait tomber le pion au dessus du précédent si jamais un pion est déjà posé sur cette colonne
                             while self.grille[current_y][int(choosen_column) - 1] == 1 or self.grille[current_y][
                                 int(choosen_column) - 1] == 2:
@@ -101,11 +153,13 @@ class Partie:
                                 current_player = 2
                             cls = lambda: print('\n' * 100)
                             cls()
+                    else:
+                        print("you have to enter a number")
                 else:
                     choosen_column = random.randint(1, self.n)
                     if (not choosen_column == "") and (not int(choosen_column) > self.m):
-                        current_y = self.m - 1
-                        # On fait tomber le pion au dessus du précédent si jamais un pion est déjà posé sur cette colonne
+                        current_y = self.n - 1
+                        # On fait tomber le pion au dessus du précédent si jamais un πon est déjà posé sur cette colonne
                         while self.grille[current_y][int(choosen_column) - 1] == 1 or self.grille[current_y][
                             int(choosen_column) - 1] == 2:
                             current_y -= 1
@@ -115,5 +169,5 @@ class Partie:
                             print("Dumb ia is playing")
                             self.combo_for_user(current_y, 2, choosen_column)
                             current_player = 1
-            else:
-                print("you have to enter a number")
+                    else:
+                        print("you have to enter a number")
